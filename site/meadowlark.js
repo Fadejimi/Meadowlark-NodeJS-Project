7119516b2c1d7db9dc3bbd32f3c69eba56e4c19e
Fadejimi <adegbulugbefadejimi@gmail.com>
2016-02-14 01:34:10 +0100

    Form handling

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
 
 //set up handlebars engine
 var handlebars = require('express3-handlebars')
@@ -23,7 +24,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(require('body-parser')());
 // test function to create weather data
 function getWeatherData(){
     return {
@@ -65,6 +66,42 @@ app.use(function(req, res, next) {
 });
 
 // routes go here
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', {
+        year: now.getFullYear(), month: now.getMonth()
+    });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err){
+            return res.redirect(303, '/error');
+        }
+        console.log('recieved fields:');
+        console.log(fields);
+        console.log('recieved fields:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
+app.get('/newsletter', function(req, res){
+    res.render('newsletter', { csrf: 'CSRF token goes here'});
+});
+
+app.post('/process', function(req, res){
+    /*console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token from hidden field: ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you');*/
+    if(req.xhr || req.accepts('json.html') ==='json'){
+        res.send({success: true});
+    } else {
+        res.redirect(303, '/thank-you');
+    }
+});
 app.get('/nusery-rhyme', function(req, res){
     res.render('nusery-rhyme');
 });
